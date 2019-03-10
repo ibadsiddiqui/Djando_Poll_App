6900836f9261d141ec836138687cc51fec48633e
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-03-10 21:36:31 +0500

    Model for questions added

--- Polling_App/polls/models.py
+++ Polling_App/polls/models.py
@@ -1,3 +1,7 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+
+class Questions(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
