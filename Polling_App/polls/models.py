d493519577852707ff76f8230247ff21c29051a9
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-03-11 23:02:11 +0500

    timezon flag added to check recently publication of document

--- Polling_App/polls/models.py
+++ Polling_App/polls/models.py
@@ -1,5 +1,8 @@
+import datetime
+
 # Create your models here.
 from django.db import models
+from django.utils import timezone
 
 
 class Questions(models.Model):
@@ -8,6 +11,9 @@ class Questions(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
