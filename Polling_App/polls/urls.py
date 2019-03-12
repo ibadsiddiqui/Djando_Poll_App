0c60ec4b1c7c78d65cddec702010b8454b838a9e
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-03-12 23:29:24 +0500

    URLs path added for result, votes and detail view of polls

--- Polling_App/polls/urls.py
+++ Polling_App/polls/urls.py
@@ -3,5 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/1
+    path('<int:quesiton_id>/', views.detail, name='detail'),
+    # ex: /polls/1/result/
+    path('<int:quesiton_id>/results/', views.results, name='result'),
+    # ex: /polls/1/votes
+    path('<int:quesiton_id>/vote/', views.vote, name='result'),
+
+
 ]
