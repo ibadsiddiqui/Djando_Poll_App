aa6ead7ca06150180f39753835c13aff1185e3c9
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-03-14 22:55:38 +0500

    namespace added for url patterns

--- Polling_App/polls/urls.py
+++ Polling_App/polls/urls.py
@@ -2,15 +2,16 @@ from django.urls import path
 
 from . import views
 
+app_name ="polls"
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/1
-    path('<int:quesiton_id>/', views.detail, name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/1/result/
-    path('<int:quesiton_id>/results/', views.results, name='result'),
+    path('<int:question_id>/results/', views.results, name='result'),
     # ex: /polls/1/votes
-    path('<int:quesiton_id>/vote/', views.vote, name='result'),
+    path('<int:question_id>/vote/', views.vote, name='result'),
 
 
 ]
