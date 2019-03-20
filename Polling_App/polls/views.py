a3bc054289b04f16ce60fd53eb82119e7525a145
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2019-03-20 23:12:18 +0500

    render error msg when no choice is selected

--- Polling_App/polls/views.py
+++ Polling_App/polls/views.py
@@ -26,3 +26,9 @@ def vote(request, question_id):
     question = get_object_or_404(Questions, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+            # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
