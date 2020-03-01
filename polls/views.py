90ffc038836a0fbda105d0786ee3433e0f38f122
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2020-03-01 13:33:30 +0500

    --Commit: template render for polls and list passed

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import render
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Salam, Ibad here")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
